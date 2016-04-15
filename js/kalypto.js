423d7715d8efbe056b552b74c4a137094dc23c0c
localpcguy <mike@local-pc-guy.com>
2016-04-15 16:20:41 -0400

    update kalypto to better reflect my curret JS standards

--- js/kalypto.js
+++ js/kalypto.js
@@ -1,4 +1,4 @@
-﻿/*! "Kalypto - Replace Radio/Checkbox Inputs" MIT license, LocalPCGuy, http://localpcguy.github.com/Kalypto */
+﻿/*! 'Kalypto - Replace Radio/Checkbox Inputs' MIT license, LocalPCGuy, http://localpcguy.github.com/Kalypto */
 /********************************
 * Kalypto - Replace checkboxes and radio buttons
 * Created & copyright (c)  by Mike Behnke
@@ -9,106 +9,121 @@
 * Released under MIT License
 *
 * usage:
-*        $("input[name=rDemo]").kalypto();
-*        $("#checkboxDemo").kalypto();
+*        $('input[name=rDemo]').kalypto();
+*        $('#checkboxDemo').kalypto();
 * events: (bound on the input)
 *        k_elbuilt: when an element is built
 *        k_checked: when an element is checked
 *        k_unchecked: when an element is checked
 ********************************/
-; (function ($, undefined) {
+;(function ($) {
+    'use strict';
 
-    $.kalypto = function (element, options) {
+    $.kalypto = Kalypto;
+    $.fn.kalypto = kalyptoPlugin;
 
-        var plugin = this,
-            $element = $(element),
-            defaults = {
-                toggleClass: "toggle",
-                checkedClass: "checked",
+    function kalyptoPlugin(options) {
+        return this.each(_attachKalypto);
+
+        function _attachKalypto() {
+            var $el = $(this);
+            if ($el.data('kalypto') === undefined) {
+                var plugin = new $.kalypto(this, options);
+                $el.data('kalypto', plugin);
+            } else {
+                console.log('Kalypto is already defined on this element.', $el);
+            }
+        }
+    }
+
+    function Kalypto(element, options) {
+        var plugin = this;
+        var $element = $(element);
+        var defaults = {
+                toggleClass: 'toggle',
+                checkedClass: 'checked',
                 hideInputs: true,
                 copyInputClasses: true,
-                dataLabel: $element.data("label") || "",
-                checkedEvent: "k_checked",
-                uncheckedEvent: "k_unchecked",
-                elBuiltEvent: "k_elbuilt",
-                customClasses: ""
-            },
-            $customEl,
-            buildCustomElement = function () {
-                if ($element.next().hasClass(plugin.settings.toggleClass)) { return; }
-                $element.after(function () {
-                    var classes = plugin.settings.toggleClass;
-                    if (plugin.settings.copyInputClasses) {
-                        var elClass = $element.attr("class");
-                        if(elClass){
-                            classes += " " + elClass;
-                        }
-                    }
-                    if (plugin.settings.customClasses.length) {
-                        classes += " " + plugin.settings.customClasses;
-                    }
-                    if ($element.is(":checked")) {
-                        return "<a href='#' class='" + classes + " " + plugin.settings.checkedClass + "'>" + plugin.settings.dataLabel + "</a>";
-                    } else {
-                        return "<a href='#' class='" + classes + "'>" + plugin.settings.dataLabel + "</a>";
-                    }
-                });
-                if (plugin.settings.hideInputs) {
-                    $element.hide();
-                }
-                $customEl = $element.next();
-                $element.trigger(plugin.settings.elBuiltEvent);
-            },
-            lastClickedEl,
-            handleChange = function (e) {
-                var $elementCollection = $element.attr("type") === "radio" ? $('input[name="' + $element.attr("name") + '"]') : $element,
-                    doTriggerAndChangeClasses = function() {
-                        if ($element.attr("type") === "radio") {
-                            $elementCollection.each(function (k, el) {
-                                var $el = $(el);
-                                $el.next().removeClass(plugin.settings.checkedClass);
-                                if (!$el.is(":checked") && plugin.lastClickedEl !== $el.next().get(0)) {
-                                    // Should this only trigger on the radio button that was previously checked
-                                    // Currently fires on all radio buttons BUT the one justed checked
-                                    $el.trigger(plugin.settings.uncheckedEvent);
-                                }
-                            });
-                        }                   
-                        if ($element.is(":checked")) { $element.trigger(plugin.settings.checkedEvent); }
-                        else { $element.trigger(plugin.settings.uncheckedEvent); }
-                        $element.next().toggleClass(plugin.settings.checkedClass);
-                    };
-                
-                if (this.tagName !== "INPUT") {
-                    e.preventDefault();
-                    plugin.lastClickedEl = this;
-                    $element.trigger('click');
-                } else {
-                    setTimeout(doTriggerAndChangeClasses, 0);
-                }
-            },
-            initEvents = function () {
-                $element.next().bind("click", handleChange);
-                $element.bind("change", handleChange);
+                dataLabel: $element.data('label') || '',
+                checkedEvent: 'k_checked',
+                uncheckedEvent: 'k_unchecked',
+                elBuiltEvent: 'k_elbuilt',
+                customClasses: ''
             };
+        var $customEl;
 
         plugin.settings = {};
-
-        plugin.init = function () {
-            plugin.settings = $.extend({}, defaults, options);
-            buildCustomElement();
-            initEvents();
-        };
-
+        plugin.init = _pluginInit;
         plugin.init();
-    };
-    $.fn.kalypto = function (options) {
-        return this.each(function () {
-            if (undefined === $(this).data('kalypto')) {
-                var plugin = new $.kalypto(this, options);
-                $(this).data('kalypto', plugin);
-            }
-        });
-    };
 
-})(jQuery);
+        function _pluginInit() {
+            plugin.settings = $.extend({}, defaults, options);
+            _buildCustomElement();
+            _initEvents();
+        }
+
+        function _buildCustomElement() {
+            if ($element.next().hasClass(plugin.settings.toggleClass)) { return; }
+            $element.after(_buildElement);
+            if (plugin.settings.hideInputs) {
+                $element.hide();
+            }
+            $customEl = $element.next();
+            $element.trigger(plugin.settings.elBuiltEvent);
+
+            function _buildElement() {
+                var classes = plugin.settings.toggleClass;
+                if (plugin.settings.copyInputClasses) {
+                    var elClass = $element.attr('class');
+                    if(elClass){
+                        classes += ' ' + elClass;
+                    }
+                }
+                if (plugin.settings.customClasses.length) {
+                    classes += ' ' + plugin.settings.customClasses;
+                }
+                if ($element.is(':checked')) {
+                    return '<a href="#" class="' + classes + ' ' + plugin.settings.checkedClass + '">' + plugin.settings.dataLabel + '</a>';
+                } else {
+                    return '<a href="#" class="' + classes + '">' + plugin.settings.dataLabel + '</a>';
+                }
+            }
+        }
+
+        function _initEvents() {
+            $element.next().bind('click', _handleChange);
+            $element.bind('change', _handleChange);
+        }
+
+        function _handleChange(e) {
+            var $elementCollection = $element.attr('type') === 'radio' ? $('input[name="' + $element.attr('name') + '"]') : $element;
+            
+            if (this.tagName !== 'INPUT') {
+                e.preventDefault();
+                plugin.lastClickedEl = this;
+                $element.trigger('click');
+            } else {
+                setTimeout(_doTriggerAndChangeClasses, 0);
+            }
+
+            function _doTriggerAndChangeClasses() {
+                if ($element.attr('type') === 'radio') {
+                    $elementCollection.each(_iterateRadioCollection);
+                }                   
+                if ($element.is(':checked')) { $element.trigger(plugin.settings.checkedEvent); }
+                else { $element.trigger(plugin.settings.uncheckedEvent); }
+                $element.next().toggleClass(plugin.settings.checkedClass);
+            }
+
+            function _iterateRadioCollection(k, el) {
+                var $el = $(el);
+                $el.next().removeClass(plugin.settings.checkedClass);
+                if (!$el.is(':checked') && plugin.lastClickedEl !== $el.next().get(0)) {
+                    // Should this only trigger on the radio button that was previously checked
+                    // Currently fires on all radio buttons BUT the one justed checked
+                    $el.trigger(plugin.settings.uncheckedEvent);
+                }
+            }
+        }
+    }
+}(jQuery));
